ae2fd95ef326aef818cc0c63b3b45693d31c058d
r3pic <101osc@naver.com>
2024-05-22 12:05:33 +0900

    fix

--- input.c
+++ input.c
@@ -11,7 +11,7 @@
 unsigned int inputdata[6] = { 0 };
 int currentDigit = 0;
 bool isDone = false;
-int current_data;
+int *current_data;
 unsigned long lastInterruptTime = 0;
 int fail_count = 0;
 
@@ -187,29 +187,29 @@ void button4_pressed()
         case IDLE:
             break;
         case INPUT_ID:
-            current_data = get_plain_int();
-            printf("input OK: %d\n", current_data);
-            if (!user_exists(db, current_data))
+            *current_data = get_plain_int();
+            printf("input OK: %d\n", *current_data);
+            if (!user_exists(db, *current_data))
             {
-                printf("User with ID %d not exists. Try again.\n", current_data);
+                printf("User with ID %d not exists. Try again.\n", *current_data);
             }
             else
             {
-                printf("User with ID %d exists.\n", current_data);
+                printf("User with ID %d exists.\n", *current_data);
                 current_State = INPUT_PASSWORD;
             }
             input_clear();
             isDone = true;
             break;
         case INPUT_PASSWORD:
-            current_data = get_plain_int();
-            printf("input OK: %d\n", current_data);
+            *current_data = get_plain_int();
+            printf("input OK: %d\n", *current_data);
             unsigned int fndData[2];
-            parseToFnd(fndData, current_data);
+            parseToFnd(fndData, *current_data);
             printf("FND Data: FND1 = %08X, FND2 = %08X\n", fndData[0], fndData[1]);
-            int password_db = get_password(db, current_data);
+            int password_db = get_password(db, *current_data);
 
-            if (password_db != -1 && password_db == current_data)
+            if (password_db != -1 && password_db == *current_data)
             {
                 printf("Password check successful.\n");
                 current_State = LOGGED_IN;
